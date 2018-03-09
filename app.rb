4af67e058a5ff55be67cd7048d2f20028e43714d
Jee Song <jeesong@Jees-MacBook-Pro.local>
2018-03-09 15:29:59 -0800

    Basic command handling

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'app/slack_authorizer'
 
 use SlackAuthorizer
 
+HELP_RESPONSE = "Use `/voybot` to see which AE owns the specific State or Country based on territories. Example: `/voybot New York`".freeze
+INVALID_RESPONSE = "Sorry, I don't recognize that city/state/country.".freeze
+
 post '/slack/territory' do
-  "OK"
+  case params['text'].to_s.strip
+  when 'help',  then HELP_RESPONSE
+  when 'NY', then "Mallorie"
+  else INVALID_RESPONSE
+  end
 end
